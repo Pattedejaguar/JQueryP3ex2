d0b76a99e41b1a67d40b98a1f4677f2053ef759b
Yannick L <lef.yannick@gmail.com>
2019-04-29 09:37:53 +0200

    commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+  var clicks = 0;
+  function more() {
+  clicks += 3.321;
+  document.getElementById("nombre").innerHTML = clicks;
+  };
+  function less() {
+  clicks -=2;
+  document.getElementById("nombre").innerHTML = clicks;
+};
